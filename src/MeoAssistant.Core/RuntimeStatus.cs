49c863c2961cd9e12bee2c9ed37421a4808f04cf
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-08-11 09:42:58 +0800

    添加 RuntimeStatus

--- src/MeoAssistant.Core/RuntimeStatus.cs
+++ src/MeoAssistant.Core/RuntimeStatus.cs
@@ -13,10 +13,72 @@
 
 namespace MeoAssistant.Core
 {
+    using MeoAssistant.Core.Utilities.Types;
+
     /// <summary>
     /// The runtime status.
     /// </summary>
     public class RuntimeStatus
     {
+        public const string RoguelikeCharElitePrefix = "RoguelikeElite-";
+
+        public const string RoguelikeCharLevelPrefix = "RoguelikeLevel-";
+
+        public const string RoguelikeCharRarityPrefix = "RoguelikeRarity-";
+
+        public const string RoguelikeCharOverview = "RoguelikeOverview";
+
+        public const string RoguelikeTraderNoLongerBuy = "RoguelikeNoLongerBuy";
+
+        private readonly Dictionary<string, long> numbers = new();
+
+        private readonly Dictionary<string, Rect> rects = new();
+
+        private readonly Dictionary<string, string> strings = new();
+
+        public long? GetNumber(string key)
+        {
+            return this.numbers.ContainsKey(key) ? this.numbers[key] : null;
+        }
+
+        public void SetNumber(string key, long value)
+        {
+            this.numbers[key] = value;
+        }
+
+        public void ClearNumbers()
+        {
+            this.numbers.Clear();
+        }
+
+        public Rect? GetRect(string key)
+        {
+            return this.rects.ContainsKey(key) ? this.rects[key] : null;
+        }
+
+        public void SetRect(string key, Rect rect)
+        {
+            this.rects[key] = rect;
+        }
+
+        public void ClearRects()
+        {
+            this.rects.Clear();
+        }
+
+        public string? GetString(string key)
+        {
+            return this.strings.ContainsKey(key) ? this.strings[key] : null;
+        }
+
+        public void SetString(string key, string value)
+        {
+            this.strings[key] = value;
+        }
+
+        public void ClearStrings()
+        {
+            this.strings.Clear();
+        }
     }
 }
